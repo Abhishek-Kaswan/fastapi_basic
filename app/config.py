ab70e63f84f4ae9e843c7326f8e96324cb5baeb3
Abhishek-Kaswan <151718252+Abhishek-Kaswan@users.noreply.github.com>
2024-12-08 22:59:14 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,17 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_HOSTNAME: str
-    DATABASE_PORT: str
-    DATABASE_PASSWORD: str
-    DATABASE_NAME: str
-    DATABASE_USERNAME: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    # DATABASE_HOSTNAME: str
+    # DATABASE_PORT: str
+    # DATABASE_PASSWORD: str
+    # DATABASE_NAME: str
+    # DATABASE_USERNAME: str
+    SECRET_KEY: str = = "kkfkjkj"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    SECRET_KEY 
+ALGORITHM = 
+ACCESS_TOKEN_EXPIRE_MINUTES = 
 
     class Config:
         env_file = ".env"
