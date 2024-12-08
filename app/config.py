6eec6e0b3c341771af87b8802b5579897230fcd0
Abhishek-Kaswan <151718252+Abhishek-Kaswan@users.noreply.github.com>
2024-12-08 22:59:44 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,9 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = = "kkfkjkj"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    SECRET_KEY 
-ALGORITHM = 
-ACCESS_TOKEN_EXPIRE_MINUTES = 
+    
 
     class Config:
         env_file = ".env"
